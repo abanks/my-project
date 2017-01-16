5d06a4c90bd8ff5bfb1e4793ac9479211a9455da
abanks <ashley_banks@davincischools.org>
2017-01-16 15:04:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,209 +1,378 @@
-(function() {
-	var canvasBody, $,
-			w = window.innerWidth,
-			h = window.innerHeight,
-			gui,
-			stats = new Stats(),
-			tick = 0,
-			pi = Math.PI,
-			pi2 = pi*2,
-			piD2 = pi/2,
-			piD3 = pi/3,
-			piD6 = pi/6,
-			pi2D3 = pi2/3,
-			piDeg = pi/180,
-			canvasStyles = {
-				position: "absolute",
-				top: "0px",
-				left: "0px",
-				"background-color": "#222",
-				"z-index": -1
-			},
-			Mouse = new Vector2(w/2, h/2),
-			mousePressed = false,
-			opts = {
-				bgc: "#222",
-				showFPS: false,
-				radius: 150,
-				maxPoints: 20,
-				startAmount: 1,
-				minPoints: 1,
-				speedLimit: 2,
-				addSpeed: 50,
-				lineWidth: 5,
-				joinStyle: "round"
-			},
-			Shape = function(X, Y){
-				this.pos = new Vector2(X, Y);
-				this.points = [];
-				this.iDir = true; //increment direction
-				this.init();
-				this.pointsA = opts.startAmount;
-			},
-			Point = function(X, Y){
-				this.pos = new Vector2(X, Y);
-				this.destiny = this.pos.copy();
-				this.speed = new Vector2();
-				this.acc = new Vector2();
-			}
-	Shape.prototype.update = function(){
-		this.points.map(function(P){
-			P.update();
-		});
-		return this;
-	};
-	Shape.prototype.addPoint = function(){
-		if(this.pointsA < opts.maxPoints){
-			this.pointsA++;
-			var length = this.points.length;
-			for(var i = 0; i < this.pointsA; i++){
-				var ang = pi2/this.pointsA*i
-				this.points[i].destiny.set(this.pos.x+Math.cos(ang)*opts.radius, this.pos.y+Math.sin(ang)*opts.radius)
-			}
-			for(var i = this.pointsA; i < length; i++){
-				this.points[i].destiny.set(this.pos.x+opts.radius, this.pos.y);
-			}
-		} else {
-			this.iDir = false;
-		}
-	};
-	Shape.prototype.removePoint = function(){
-		if(this.pointsA > opts.minPoints){
-			this.pointsA--;
-			var length = this.points.length,
-					pA = this.pointsA;
-			for(var i =0; i < pA; i++){
-				var ang = pi2/pA*i;
-				this.points[i].destiny.set(this.pos.x+Math.cos(ang)*opts.radius, this.pos.y+Math.sin(ang)*opts.radius);
-			}
-			for(var i = pA; i < length; i++){
-				this.points[i].destiny.set(this.pos.x+opts.radius, this.pos.y);
-			}
-		} else {
-			this.iDir = true;
-		}
-	};
-	Shape.prototype.init = function(){
-		for(var i = 0; i < opts.maxPoints; i++){
-			var ang = pi2/opts.startAmount;
-			this.points.push(new Point(this.pos.x+Math.cos(ang*i)*opts.radius,this.pos.y+Math.sin(ang*i)*opts.radius));
-		};
-	}
-	Shape.prototype.render = function(){
-		$.beginPath();
-		$.moveTo(this.pos.x + this.radius, this.pos.y);
-		for(var i = 0; i < this.points.length; i++){
-			$.lineTo(this.points[i].pos.x, this.points[i].pos.y);
-		}
-		
-		var gradient = $.createLinearGradient(w/2+opts.radius, h/2+opts.radius, w/2-opts.radius, h/2-opts.radius);
-		for(var i = 0; i < this.points.length; i++){
-			var length = this.points.length;
-			var ang = (pi2/length)*i*180/pi;
-			gradient.addColorStop(i/length, "hsl("+ang+", 50%, 50%)");
-		}
-		$.closePath();
-		$.strokeStyle = gradient;
-		$.lineWidth = opts.lineWidth;
-		$.lineJoin = opts.joinStyle;
-		$.stroke();
-	}
-	 
-	Point.prototype.update = function(){
-		this.attractTo(this.destiny);
-		this.speed.add(this.acc);
-		this.pos.add(this.speed);
-		this.acc.set(0);
-		this.gonnaGetRemoved && this.pos.distanceTo({x: opts.radius+w/2, y:h/2}) < 1 ? shape.points.pop() : undefined;
-		return this;
-	}
-	Point.prototype.attractTo = function(target){
-		var tar = target.copy();
-		var distance = this.pos.distanceTo(target);
-		tar.sub(this.pos);
-		tar.limit(distance/2);
-		
-		var des = tar.sub(this.speed);
-		des.limit(2);
-		this.acc.add(tar);
-	}
-	function setup(){
-		createCanvas();
-		addListeners();
-		shape = new Shape(w/2, h/2);
-		gui = new dat.GUI();
-		gui.close();
-		gui.add(opts,"addSpeed", 0, 60, 1);
-		gui.add(opts,"lineWidth",0.5, 50,1,0.5);
-		gui.add(opts,"joinStyle",["round", "bevel", "miter"]);
-		document.body.appendChild(stats.domElement);
-		window.requestAnimationFrame(loop);
-	}
-	function loop(){
-		stats.begin();
-		drawBg();
-		
-		tick++;
-		tick%opts.addSpeed==0?(shape.iDir?shape.addPoint():shape.removePoint()):undefined
-		shape.update().render();
-		
-		opts.showFPS ? stats.domElement.style["display"] = "block" : stats.domElement.style["display"] = "none";
-		window.requestAnimationFrame(loop);
-		stats.end();
-	}
-	function createCanvas() {
-		var el = document.createElement("canvas"),
-				ctx = el.getContext("2d");
-		for (var style in canvasStyles) {
-			el.style[style] = canvasStyles[style];
-		}
-		document.body.appendChild(el);
-		canvasBody = el;
-		$ = ctx;
-		canvasBody.width = w;
-		canvasBody.height = h;
-		document.body.style["overflow"] = "hidden"
-		return [el, ctx];
-	}
-	function drawBg() {
-		$.fillStyle = opts.bgc;
-		$.fillRect(0, 0, w, h);
-	}
-	
-	function addListeners(){
-		window.addEventListener("resize", resize);
-		window.addEventListener("mousemove", mouseMove);
-		canvasBody.addEventListener("mousedown", mouseDown);
-		canvasBody.addEventListener("mouseup", mouseUp);
-		canvasBody.addEventListener("touchmove", touchMove);
-		canvasBody.addEventListener("touchstart", touchStart);
-		canvasBody.addEventListener("touchend", touchEnd);
-	}
-	function resize(){
-		w = canvasBody.width = window.innerWidth;
-		h = canvasBody.height = window.innerHeight;
-	}
-	function mouseDown(event){
-		mousePressed = true;
-		Mouse.set(event.pageX, event.pageY);
-	}
-	function mouseUp(event){
-		mousePressed = false;
-	}
-	function mouseMove(event){
-		Mouse.set(event.pageX, event.pageY);
-	}
-	function touchMove(event){
-		var touches = event.changedTouches;
-		Mouse.set(touches[0].pageX, touches[0].pageY);
-	}
-	function touchStart(event){
-		e.preventDefault();
-		mousePressed = true;
-		touchMove(event);
-	}
-	function touchEnd(event){
-		mousePressed = false;
-	}
-	setup();
-})()
+body {
+  margin: 0;
+}
+header {
+  width: 100%;
+  background-color: #fff;
+  color: #5f6db0; /* Pantone Baja Blue */ 
+  padding-top: 1em;
+}
+footer{
+  background-color: #5f6db0;
+  color: #fff;
+  width: 100%;
+  padding-bottom: 1.5em;
+}
+h5{
+  padding-top: 1.5em;
+}
+
+.container{
+  background-color: #fff;
+  margin: 0 auto;
+  min-width: 95%;
+}
+.palette{
+  margin-left: 1.5rem;
+}
+.material-icons{
+  font-size: 0.75em;
+  margin-bottom: 0.25em;
+  vertical-align: bottom;
+}
+.white-icon{
+  color: #fff;
+  font-size: 1em;
+}
+.color{
+  display: inline-block;
+  min-height: 2em;
+  min-width: 2em;
+  border-radius: 100%;
+  margin: 1em;
+}
+.color-sm{
+  display: inline-block;
+  min-height: 1.5em;
+  min-width: 1.5em;
+  border-radius: 100%;
+  margin: 0.75em;
+}
+
+.ant-a{
+  background-color: #87d068;
+}
+.ant-b{
+  background-color: #f50;
+}
+.ant-c{
+  background-color: #d9d9d9;
+}
+.ant-d{
+  background-color: #108ee9;
+}
+.ant-e{
+  background-color: #fa0;
+}
+.bulma-a{
+  background-color: #00d1b2
+}
+.bulma-b{
+  background-color: #3273dc
+}
+.bulma-c{
+  background-color: #23d160
+}
+.bulma-d{
+  background-color: #ffdd57
+}
+.bulma-e{
+  background-color: #ff3860
+}
+.bootstrap-a {
+  background-color: #0275d8;
+}
+.bootstrap-b {
+  background-color: #5cb85c;
+}
+.bootstrap-c {
+  background-color: #5bc0de;
+}
+.bootstrap-d {
+  background-color: #f0ad4e;
+}
+.bootstrap-e {
+  background-color: #d9534f;
+}
+.bootstrap-f {
+  background-color: #ffd500;
+}
+.bootstrap-g {
+  background-color: #ff5b77;
+}
+.bootstrap-h {
+  background-color: #613d7c;
+}
+.bootstrap-i {
+  background-color: #292b2c;
+}
+.bootstrap-j {
+  background-color: #464a4c;
+}
+.bootstrap-k {
+  background-color: #636c72;
+}
+.bootstrap-l {
+  background-color: #eceeef;
+}
+.bootstrap-m {
+  background-color: #f7f7f9;
+}
+.concise-a{
+  background-color: #c65146;
+}
+.concise-b{
+  background-color: #f17f42;
+}
+.concise-c{
+  background-color: #ebe1d3;
+}
+.concise-d{
+  background-color: #dcdcdc;
+}
+.concise-f{
+  background-color: #32373d;
+}
+.concise-g{
+  background-color: #444;
+}
+.concise-j{
+  background-color: #f5f5f5;
+}
+.concise-l{
+  background-color: #4d4d4d;
+}
+.concise-m{
+  background-color: #666;
+}
+.concise-n{
+  background-color: #808080;
+}
+.concise-p{
+  background-color: #1573b6;
+}
+.concise-q{
+  background-color: #1e80c6;
+}
+.concise-r{
+  background-color: #2b90d9;
+}
+.concise-s{
+  background-color: #3fa2e9;
+}
+.concise-t{
+  background-color: #089073;
+}
+.concise-u{
+  background-color: #0b9d73;
+}
+.concise-v{
+  background-color: #10a887;
+}
+.concise-w{
+  background-color: #1eb896;
+}
+.concise-x{
+  background-color: #28ceaa;
+}
+.concise-y{
+  background-color: #653131;
+}
+.concise-z{
+  background-color: #b73333;
+}
+.concise-1{
+  background-color: #da3c3c;
+}
+.concise-2{
+  background-color: #f25a5a;
+}
+.concise-3{
+  background-color: #fa8181;
+}
+.kube-a{
+  background-color: #f4f4f4;
+}
+.kube-b{
+  background-color: #3c74d9;
+}
+.kube-c{
+  background-color: #60ae4d;
+}
+.kube-d{
+  background-color: #f34248;
+}
+.kube-e{
+  background-color: #fed443;
+}
+.kule-a{
+  background-color: #45a7b9;
+}
+.kule-b{
+  background-color: #55b3e9;
+}
+.kule-c{
+  background-color: #f45b8a;
+}
+.kule-d{
+  background-color: #374e52;
+}
+.kule-e{
+  background-color: #82ba29;
+}
+.kule-f{
+  background-color: #f38b2e;
+}
+.kule-g{
+  background-color: #d63a2b;
+}
+.kule-h{
+  background-color: #b3b3b3;
+}
+.mdb-a{
+  background-color: #f44;
+}
+.mdb-b{
+  background-color: #c00;
+}
+.mdb-c{
+  background-color: #fb3;
+}
+.mdb-d{
+  background-color: #f80;
+}
+.mdb-e{
+  background-color: #00c851;
+}
+.mdb-f{
+  background-color: #007e33;
+}
+.mdb-g{
+  background-color: #33b5e5;
+}
+.mdb-h{
+  background-color: #09c;
+}
+.mdb-i{
+  background-color: #2bbbad;
+}
+.mdb-j{
+  background-color: #00695c;
+}
+.mdb-k{
+  background-color: #4285f4;
+}
+.mdb-l{
+  background-color: #0d47a1;
+}
+.mdb-m{
+  background-color: #a6c;
+}
+.mdb-n{
+  background-color: #93c;
+}
+.mdb-o{
+  background-color: #2e2e2e;
+}
+.mdb-p{
+  background-color: #212121;
+}
+.mdb-q{
+  background-color: #4b515d;
+}
+.mdb-r{
+  background-color: #3e4551;
+}
+.mdb-s{
+  background-color: #3f729b;
+}
+.mdb-t{
+  background-color: #1c2331;
+}
+.mdb-u{
+  background-color: #37474f;
+}
+.mdb-v{
+  background-color: #263238;
+}
+.schema-a{
+  background-color: #3498db;
+}
+.schema-b{
+  background-color: #2ecc71;
+}
+.schema-c{
+  background-color: #24cabc;
+}
+.schema-d{
+  background-color: #f39c12;
+}
+.schema-e{
+  background-color: #374c3c;
+}
+.schema-f{
+  background-color: #000;
+}
+.schema-g, .concise-e{
+  background-color: #222;
+}
+.schema-h, .concise-k{
+  background-color: #333;
+}
+.schema-i{
+  background-color: #555;
+}
+.schema-j, .concise-h{
+  background-color: #777;
+}
+.schema-k, .concise-o{
+  background-color: #999;
+}
+.schema-l{
+  background-color: #aaa;
+}
+.schema-m{
+  background-color: #ccc;
+}
+.schema-n{
+  background-color: #ddd;
+}
+.schema-o, .concise-i{
+  background-color: #eee;
+}
+.sierra-a{
+  background-color: #64829f;
+}
+.sierra-b{
+  background-color: #34495e;
+}
+.sierra-c{
+  background-color: #1abc9c;
+}
+.sierra-d{
+  background-color: #9b59b6;
+}
+.sierra-e{
+  background-color: #18232f;
+}
+.sierra-f{
+  background-color: #4caf50;
+}
+.sierra-g{
+  background-color: #f0ad4e;
+}
+.sierra-h{
+  background-color: #374c3c;
+}
+.spectre-a{
+  background-color: #efefef;
+}
+.spectre-b{
+  background-color: #5764c6;
+}
+.spectre-c{
+  background-color: #32b643;
+}
+.spectre-d{
+  background-color: #e85600;
+}
