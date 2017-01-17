c6012a7efbe87fa96b5baafd0bed3c93fad78d5f
abanks <ashley_banks@davincischools.org>
2017-01-16 16:05:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$( "button" ).click(function() {
-  $(.this).toggleClass( "active" );
-  $("icons").toggleClass( "open" );
+$( ".button" ).click(function() {
+  $(this).toggleClass( "active" );
+  $(".icons").toggleClass( "open" );
  
 });
