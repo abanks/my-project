4d6abe69441dd96a81d3b98361a2472340a161f8
abanks <ashley_banks@davincischools.org>
2017-01-16 16:00:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
-$(document).ready(function() {
-	var toggleCat = "#toggle_menu_category";
-	var menuCat = "#menu_category";
-	
-	var toggle2 = "#toggle_menu_2"
-	var menu2 = "#menu_2";
-	
-	var toggleMain = "#toggle_menu_main";
-	var menuMain = "#menu_main";
-	
-	var overlay = ".site-overlay";
-	
-	$(toggleCat).click(function() {
-		$(menuCat).addClass("open");
-		$(overlay).css("display", "block")
-	});
-	
-	$(toggle2).click(function() {
-		$(menu2).addClass("open");
-		$(overlay).css("display", "block")
-	});
-	
-	$(toggleMain).click(function() {
-		$(menuMain).addClass("open");
-		$(overlay).css("display", "block");
-	});
-	
-	$(overlay).click(function() {
-		$(menuCat).removeClass("open");
-		$(menu2).removeClass("open");
-		$(menuMain).removeClass("open");
-		$(this).css("display", "none");
-	})
+$( ".button" ).click(function() {
+  $(this).toggleClass( "active" );
+  $(".icons").toggleClass( "open" );
+ 
 });
