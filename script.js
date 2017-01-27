f1f73e0316d1c085ab9c9c48714af867f2f8cde5
abanks <ashley_banks@davincischools.org>
2017-01-27 08:45:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+ // This code is for the nav bar at the top of the screen and this code pertains to the scrollbar while you are scrolling down the page
 window.onscroll = function() {
   var el = document.getElementsByClassName('header')[0];
   var className = 'small';
